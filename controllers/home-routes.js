a7228f0a63db1d638debc449bc9bdf99c840d139
Amir <amir.hackett@gmail.com>
2022-05-24 16:53:59 -0400

    added login and logout capablities

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,7 +29,6 @@ router.get('/', (req, res) => {
     ]
   })
     .then(dbPostData => {
-      // entire array of posts object into the homepage template
       const posts = dbPostData.map(post => post.get({ plain: true }));
 
       res.render('homepage', { posts });
@@ -38,6 +37,15 @@ router.get('/', (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
+});
+
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
 });
 
 module.exports = router;
